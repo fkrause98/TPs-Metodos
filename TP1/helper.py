d90571dc01caa44ad781fe908ec5450685449707
Francisco Krause Arnim <fkrausear@gmail.com>
2022-09-02 19:37:20 -0300

    Fix python input parser

--- TP1/helper.py
+++ TP1/helper.py
@@ -1,13 +1,16 @@
 import os
 from typing import List
 
+def create_pair(pair: str):
+    i, j = tuple(pair.split(' '))
+    return (float(i), float(j))
+
 class Test:
-    def __init__(self, test_list: List[str], test_name: str) -> Test:
+    def __init__(self, test_list: List[str], test_name: str):
         n, m, *links = test_list
-        tuple_from_pair = lambda string: (int(string[0]), int(string[2]))
         self.n = n
         self.m = m
-        self.links = list(map(tuple_from_pair, links))
+        self.links = list(map(create_pair, links))
         self.name = test_name
     def __str__(self) -> str:
         tuple_to_string = lambda tuple: f"{str(tuple[0])} {str(tuple[1])} \n"
@@ -26,6 +29,10 @@ class Test:
 
 def read_test(test_path: str, test_name: str) -> Test:
     with open(test_path) as test:
-        lines: TextIOWrapper = test.readlines()
+        lines = test.readlines()
         lines = [line.rstrip() for line in lines]
     return Test(lines, test_name)
+
+def page_rank(test_path: str, test_name: str):
+       test: Test = read_test(test_path, test_name)
+       return 1
